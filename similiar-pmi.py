8abf481a6081cccfab63563ba962ec495bb6db09
Joseph Hnatek <hnate002@d.umn.edu>
2020-11-09 22:52:51 -0600

    posible completion

--- similiar-pmi.py
+++ similiar-pmi.py
@@ -11,7 +11,7 @@ def preProcessing(filepath):
 
     directory = filepath
     for filename in os.listdir(filepath):
-        print(filename)
+        #print(filename)
         if filename.endswith(".txt"):
             with open(filepath + '/' + filename, 'r') as file:
                 data = file.read()
@@ -88,7 +88,8 @@ def findCoWords(data, windowSize):
     if windowSize == 2:
         result = bigram(data)
     elif windowSize == 5:
-        result = fiveGram(data)
+        #result = fiveGram(data)
+        pass
     else:
         print("unknown error")
         exit()
@@ -147,16 +148,17 @@ def getCoWordCount(data):
 def createMatrix(data):
 
     dim = len(data)
-    print(dim)
+    #print(dim)
     
     pmiMatrix = numpy.zeros((dim, dim), int) # target (row) x context (col)
 
     PMI_INDEX = {}
-
+    INVERSE = {}
     index = 0
 
     for word in data:
         PMI_INDEX[word] = index
+        INVERSE[index] = word
         index += 1
 
     #print(PMI_INDEX)
@@ -165,10 +167,9 @@ def createMatrix(data):
 
     #print(pmiMatrix)
 
-    return pmiMatrix, PMI_INDEX
+    return pmiMatrix, PMI_INDEX, INVERSE
 
 def calculatePMI(data, singleWordsCounts, totalCount, pmiMatrix, PMI_INDEX):
-
 
     for target in singleWordsCounts:
         for context in singleWordsCounts:
@@ -180,9 +181,73 @@ def calculatePMI(data, singleWordsCounts, totalCount, pmiMatrix, PMI_INDEX):
                     p1 = singleWordsCounts[target] / totalCount
                     p2 = singleWordsCounts[context] / totalCount
 
+                    #if PMI_INDEX[target] > 9999 or PMI_INDEX[context] > 9999:
+                     #   break
                     pmiMatrix[PMI_INDEX[target]][PMI_INDEX[context]] = math.log2(p/(p1*p2))
 
     return pmiMatrix
+
+
+def computeCosine(pmiMatrix, PMI_INDEX, wordPairs, coWordCount, INVERSE):
+    
+    cosine = {}
+    print(coWordCount)
+    for pair in wordPairs:
+
+        target = pair[0]
+        context = pair[1]
+
+        if target not in PMI_INDEX or context not in PMI_INDEX:
+            cosine[tuple(pair)] = -999
+            continue
+
+        """if PMI_INDEX[target] > 10000 or PMI_INDEX[context] > 10000:
+            cosine[tuple(pair)] = 69
+            continue"""
+        
+        targetIndexRow = PMI_INDEX[target]
+        contextIndexRow = PMI_INDEX[context]
+
+        countNum = 0
+        countTargetDen = 0
+        countContextDen = 0
+
+        for i in range(len(pmiMatrix)):
+            targetRowValue = pmiMatrix[targetIndexRow][i]
+            contextRowValue = pmiMatrix[contextIndexRow][i]
+            
+            countNum += targetRowValue * contextRowValue
+
+            temp1 = INVERSE[targetIndexRow]
+            #print(INVERSE)
+            temp2 = INVERSE[i]
+            temp3 = INVERSE[contextIndexRow]
+            #print("temp2: ", temp2)
+
+            if temp1 not in coWordCount or temp2 not in coWordCount[temp1]:
+                val1 = 0
+            else:
+                val1 = coWordCount[temp1][temp2]
+
+            if temp3 not in coWordCount or temp2 not in coWordCount[temp3]:
+                val3 = 0
+            else:
+                val3 = coWordCount[temp3][temp2]
+            
+            countTargetDen += val1**2
+            
+            countContextDen += val3**2
+
+            #print(countContextDen)
+
+        #print(countTargetDen, countContextDen)
+        pairCosineValue = countNum / (math.sqrt(countTargetDen) * math.sqrt(countContextDen))
+
+        cosine[tuple(pair)] = pairCosineValue
+
+    print(cosine)
+
+
 
 def main(filepath, windowSize, wordPairsFile):
     
@@ -196,15 +261,19 @@ def main(filepath, windowSize, wordPairsFile):
     coWords = findCoWords(data, windowSize)
 
     coWordCount = getCoWordCount(coWords)
+    pmiMatrix, PMI_INDEX, INVERSE = createMatrix(singleWordsCounts)
 
-    pmiMatrix, PMI_INDEX = createMatrix(singleWordsCounts)
+    pmiMatrixTrain = calculatePMI(coWordCount, singleWordsCounts, totalCount, pmiMatrix, PMI_INDEX)
 
-    pmiMatrix = calculatePMI(coWordCount, singleWordsCounts, totalCount, pmiMatrix, PMI_INDEX)
+    #print(pmiMatrix[PMI_INDEX["clinton"]][PMI_INDEX["clinton"]])
 
-    print(pmiMatrix)
+    computeCosine(pmiMatrix, PMI_INDEX, wordPairs, coWordCount, INVERSE)
+
+    #print(PMI_INDEX["clinton"])
+
 
 if __name__ == "__main__":
-    print(sys.argv)
+    #print(sys.argv)
 
     windowSize = int(sys.argv[1])
     filepath = sys.argv[2]
